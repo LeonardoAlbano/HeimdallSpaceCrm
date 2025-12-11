2aee7b88eeb12b5f5744d0c492f3ef64d8e4658c
leonardoalbano <leonardoalbanom@yahoo.com.br>
2025-12-11 11:27:01 -0300

    feat(communication): add auth register request and response

---
+++ src/HeimdallSpaceCrm.Communication/Auth/Register/Requests/RegisterUserRequest.cs
@@ -0,0 +1,3 @@
+namespace HeimdallSpaceCrm.Communication.Auth.Register.Requests;
+
+public record RegisterUserRequest(string Name, string Email, string Password);
---
+++ src/HeimdallSpaceCrm.Communication/Auth/Register/Responses/RegisterUserResponse.cs
@@ -0,0 +1,3 @@
+namespace HeimdallSpaceCrm.Communication.Auth.Register.Responses;
+
+public record RegisterUserResponse(Guid Id, string Name, string Email);
